91691b753f9e4aafc79b4387f8143dca2a13acbe
Hamid Iqball <hamid.iqbal00123@gmail.com>
2024-04-23 08:37:25 +0500

    Made a completely Reuseable star component, that can be used in our usePopcorn project

--- src/index.js
+++ src/index.js
@@ -4,25 +4,32 @@ import ReactDOM from "react-dom/client";
 // import App from "./App";
 import StarRating from "./StarRating";
 // import StarRating from "./StarRating";
-
-// function Test() {
-//   const [movieRating, setMovieRating] = useState(0);
-//   return (
-//     <div>
-//       <StarRating color="red" maxRating={10} onSetRating={setMovieRating} />
-//       <p>This movie was rated {movieRating} stars</p>
-//     </div>
-//   );
-// }
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onRating={setMovieRating} />
+      <p> This Movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* {/<StarRating
-      maxRating={48}
+    {/* <StarRating
+      maxRating={5}
+      color="red"
+      size={48}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     /> */}
-    {/* <StarRating maxRating={10} color="#fcc419" size={24} defaultRating={0} /> */}
-    <StarRating maxRating={5} />
+    {/* <StarRating
+      maxRating={10}
+      color="#fcc419"
+      size={48}
+      className="test"
+      defaultRating={3}
+    /> */}
+    <Test />
   </React.StrictMode>
 );
